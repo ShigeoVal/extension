9816be83d1c3564c187ed6fb2d26efcb2256366d
ShigeoVal <109927254+ShigeoVal@users.noreply.github.com>
2022-07-25 20:13:59 +0100

    Create extension.user.js

---
+++ extension.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         VLR.GG EXTENSION
+// @version      0.0.1
+// @description  ADDS EMOTES
+// @author       Shigeo
+// @match        https://*.vlr.gg/*
+// @grant        GM.xmlHttpRequest
+// ==/UserScript==
+
+GM.xmlHttpRequest({
+	method: "GET",
+	url: `https://raw.githubusercontent.com/ShigeoVal/extension/main/extension.js?${Date.now}`,
+	nocache: true,
+	onload: ({ responseText }) => {
+		Function(responseText)();
+	},
+});
